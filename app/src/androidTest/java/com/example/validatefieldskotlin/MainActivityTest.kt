58518a836e208d7a8c46c582f4e9526a82362cca
Rosana Bandeira <rosanabandeira@nbbv027697.local>
2019-03-26 17:01:53 -0300

    testes

--- app/src/androidTest/java/com/example/validatefieldskotlin/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/validatefieldskotlin/MainActivityTest.kt
@@ -4,14 +4,14 @@ import android.app.LauncherActivity
 import android.os.Parcel
 import android.os.Parcelable
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.closeSoftKeyboard
-import android.support.test.espresso.action.ViewActions.typeText
+import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.support.v4.app.AppLaunchChecker
 import android.widget.EditText
-import androidx.test.rule.ActivityTestRule
 import org.junit.After
 import org.junit.Before
 
@@ -22,11 +22,11 @@ import org.junit.runner.RunWith
 
 
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class MainActivityTest {
 
-    @Rule
-     var activityTestRule = ActivityTestRule(MainActivity::class.java)
-
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
 
 
     @Before
@@ -45,6 +45,9 @@ class MainActivityTest {
     fun onView() {
 
         onView(withId(R.id.editTextId)).perform(typeText("179.565.138-55"))
+        onView(withId(R.id.editTextPhone)).perform(typeText("111111111111"))
+        onView(withId(R.id.editTextEmail)).perform(typeText("rosana@brq.com.br"))
+        onView(withId(R.id.button)).perform(click())
 
 
     }
